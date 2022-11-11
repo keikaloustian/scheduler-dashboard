8cfedf6e9dd6cf6bab7a899cfd1f5606d233f84c
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-10 18:27:54 -0800

    Connect to local storage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,85 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
   };
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel(id) {
+    this.setState((prev) => ({
+      focused: prev.focused ? null : id,
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panelsArray = (
+      this.state.focused
+        ? data.filter((panel) => this.state.focused === panel.id)
+        : data
+    ).map((datum) => {
+      return (
+        <Panel
+          key={datum.id}
+          label={datum.label}
+          value={datum.value}
+          onSelect={(event) => this.selectPanel(datum.id)}
+        />
+      );
+    });
+
+    return <main className={dashboardClasses}>{panelsArray}</main>;
   }
 }
 
